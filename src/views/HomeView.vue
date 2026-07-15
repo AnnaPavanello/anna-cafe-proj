39cff64ac79f0ce6ed491fbdd7309d3293abf4a5
AnnaPavanello <annaclaramorettipavanello@gmail.com>
2026-07-15 15:15:13 -0300

    alterações no site

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
   <main>
-    <h1>Olá, café!</h1>
+    <div class="hero">
+      <div class="hero-texto">
+        <h1>Campeonato de café</h1>
+        <p>Torneio de alguma coisa de café</p>
+      </div>
+      <div class="hero-imagem">
+        <img src="@/assets/img/images.jpeg">
+      </div>
+    </div>
+    <button class="button">
+      <RouterLink to="/ranking" class="text-button">Ranking</RouterLink>
+    </button>
   </main>
 </template>
 
@@ -8,4 +19,29 @@
 </script>
 
 <style scoped>
+.button{
+  color: #fff;
+  text-decoration: none;
+  border: 1px solid yellow;
+  border-radius: 10px;
+  background-color: brown;
+}
+.button-hover{
+  background-color: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+  transition: all 0.3s;
+  transform: translateY(-2px);
+}
+.text-button{
+  text-decoration: none;
+  color: yellow;
+  text-transform: uppercase;
+}
+.hero{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 2rem;
+}
 </style>
--- src/views/RankingView.vue
+++ src/views/RankingView.vue
@@ -1,9 +1,60 @@
 <template>
-    <h1>Ranking</h1>
-    <p>devo exibir a página de ranking</p>
+    <div class="ranking-view">
+        <h1>{{ titulo }}</h1>
+        <p>{{ texto }}</p>
+
+        <ul>
+            <li>Café Arabe - Fazenda água doce - 9.1</li>
+            <li>Café Japones - Fazenda água roxa - 8.4</li>
+            <li>Café Portugues - Fazenda água dourada - 7.1</li>
+            <li>Café Sueco - Fazenda água vermelha - 3.1</li>
+        </ul>
+        <ul>
+            <li v-for="cafe in cafesOrdenados" :key="cafe.id">
+                Nome:{{ cafe.nome }}
+                Produtor:{{ cafe.local }}
+                Media:{{ cafe.nota }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
+    import{ computed, ref } from 'vue'
+    const titulo = 'Ranking Geral'
+    const descricao = 'Confira o ranking dos melhores avaliados'
+
+    const cafes = ref([
+    {
+        id: 1,
+        nome:'Café Arabe',
+        local:'Fazenda Água Doce',
+        nota:'9.1'
+    },
+    {
+        id: 2,
+        nome:'Café Japones',
+        local:'Fazenda Água Roxa',
+        nota:'8.4'
+    },
+    {
+        id: 3,
+        nome:'Café Portugues',
+        local:'Fazenda Água Dourada',
+        nota:'7.1'
+    },
+    {
+        id: 4,
+        nome:'Café Sueco',
+        local:'Fazenda Água Vemelha',
+        nota:'3.1'
+    }
+]);
+
+const cafesOrdenados = computed(() => {
+    return [...cafes.value].sort((a,b) => b.nota - a.nota)
+});
+
 </script>
 
 <style scoped>
